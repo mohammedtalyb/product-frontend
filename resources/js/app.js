5b8808672322095789350d90a657009c5a9ac0e5
mohammedtalyb <mohammed.talib@raweng.com>
2019-05-15 17:29:30 +0530

    layout

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,12 @@
 function clickMenu() {
     var menuBar = document.querySelector('#menu');
+    var menuButton = document.querySelector('#menuButton');
     menuBar.classList.toggle('d-inline-block');
+    if(menuButton.classList.contains( "ion-ios-menu")) {
+        menuButton.classList.remove('ion-ios-menu');
+        menuButton.classList.add('ion-ios-close');
+    } else {
+        menuButton.classList.remove('ion-ios-close');
+        menuButton.classList.add('ion-ios-menu');
+    }
 }
